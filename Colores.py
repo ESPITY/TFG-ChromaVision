0b6e94045d8a74438a3b48a0138337d1ee139dde
ESPITY <132686177+ESPITY@users.noreply.github.com>
2026-02-25 01:26:03 +0100

    Colores piezas (amarillo, rojo, azul, verde y negro)

--- Colores.py
+++ Colores.py
@@ -5,8 +5,21 @@ import numpy as np
 # HSV -> Hue-X (0-180) Saturation-Y (0-255) Value (0-255)
 
 # Rango para detectar el color (HSV) -> Value (20-255) normalmente
-min = np.array([15, 150, 20])
-max = np.array([35, 255, 255])
+# Rojo: d50f07 - (2, 96.7, 83.5)
+r_lower = np.array([0, 200, 20])
+r_upper = np.array([5, 255, 255])
+# Amarillo: ffd322 - (48, 86, 100)
+y_lower = np.array([20, 200, 20])
+y_upper = np.array([25, 255, 255])
+# Azul: 0058b7 - (211, 100, 71)
+b_lower = np.array([105, 200, 20])
+b_upper = np.array([115, 255, 255])
+# Verde: 33823d - (128, 60.8, 51)
+g_lower = np.array([40, 150, 20])
+g_upper = np.array([100, 255, 255])
+# Negro: 141616 - (180, 9.1, 8.6)
+n_lower = np.array([0, 0, 0])
+n_upper = np.array([180, 255, 30])
 
 # Webcam
 stream =  cv2.VideoCapture(0)
@@ -17,7 +30,7 @@ while (True):
 
     frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # Convertir el frame de BGR a HSV
 
-    mask = cv2.inRange(frame_HSV, min, max) # Máscara del color
+    mask = cv2.inRange(frame_HSV, n_lower, n_upper) # Máscara del color
 
 
     # Obtener la posición del "objeto" y dibujar un cuadrado alrededor
