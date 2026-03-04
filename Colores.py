1a413d23711e2ae650d2aa68a8f1c8dc2205b21d
ESPITY <132686177+ESPITY@users.noreply.github.com>
2026-03-04 01:01:37 +0100

    Detección de base (contorno)

--- Colores.py
+++ Colores.py
@@ -14,8 +14,45 @@ COLORS = [
     ["Negro", np.array([0, 0, 0]), np.array([180, 255, 30]), (105, 105, 105)]
 ]
 
-MIN_AREA = 10000  # Tamaño mínimo del contorno de color detectado
+BASE_SIZE_MM = 250  # Tamaño de la base
+MIN_PIECE_AREA = 1000  # Tamaño mínimo del contorno de color detectado
 IMG_SCALE = 1 # Ver las ventanas/frames a menor tamaño (0.4)
+
+def detect_base(frame_grey):
+    frame_grey = cv2.bilateralFilter(frame_grey, 20, 30, 30)
+    edges = cv2.Canny(frame_grey, 10, 20)
+
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    edges_color = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
+
+    if contours:
+        biggest = max(contours, key=cv2.contourArea)
+
+        if cv2.contourArea(biggest) > 1000:
+            rect = cv2.minAreaRect(biggest)
+            box = cv2.boxPoints(rect)
+            box = np.intp(box)
+            cv2.drawContours(edges_color, [box], -1, (0,255,0), 3)
+
+    # 🔥 SIEMPRE devolvemos imagen
+    return edges_color
+
+# Obtiene el contorno más grande con 4 esquinas
+def biggest_contour(contours):
+    biggest = np.array([])  # contorno y área máxima
+    max_area = 0
+
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > 1000:
+            perimeter = cv2.arcLength(c, True)  # Contorno cerrado
+            approx = cv2.approxPolyDP(c, 0.012 * perimeter, True)   # Aproximarlo a otra forma (precisiónm, contorno cerrado)
+            if area > max_area and len(approx) == 4:   # Supera el a´rea máxima y tiene 4 esquinas
+                biggest = approx
+                max_area = area
+    return biggest
+
 
 # Función de detección de color
 def detect_color (frame, frame_HSV, name, lower, upper, colorBGR):
@@ -28,8 +65,8 @@ def detect_color (frame, frame_HSV, name, lower, upper, colorBGR):
     # Posición de los contornos (si hay)
     if len(mask_contours) != 0:
         for mask_contour in mask_contours:
-            if cv2.contourArea(mask_contour) > MIN_AREA: # El contorno tiene que tener un tamaño mínimo
-                # 5. Centro con momentos
+            if cv2.contourArea(mask_contour) > MIN_PIECE_AREA: # El contorno tiene que tener un tamaño mínimo
+                # Centro (momentos)
                 M = cv2.moments(mask_contour)
                 if M["m00"] == 0:
                     continue
@@ -37,7 +74,7 @@ def detect_color (frame, frame_HSV, name, lower, upper, colorBGR):
                 cx = int(M["m10"] / M["m00"])
                 cy = int(M["m01"] / M["m00"])
 
-                # 6. Orientación
+                # Orientación
                 rect = cv2.minAreaRect(mask_contour)
                 (x, y), (w, h), angle = rect
 
@@ -45,7 +82,7 @@ def detect_color (frame, frame_HSV, name, lower, upper, colorBGR):
                 if w < h:
                     angle = angle + 90
 
-                 # 7. Dibujar resultados
+                 # Dibujar
                 box = cv2.boxPoints(rect)
                 box = np.intp(box)
 
@@ -61,6 +98,7 @@ def detect_color (frame, frame_HSV, name, lower, upper, colorBGR):
     
     return mask
 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 print("Inciando webcam...")
 
@@ -78,7 +116,6 @@ stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 #stream.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 #stream.set(cv2.CAP_PROP_FOCUS, 5)
-print(stream.get(cv2.CAP_PROP_FOCUS))
 
 # Obtener todos los frames
 while (True):
@@ -87,16 +124,23 @@ while (True):
     if not success:
         break
 
-    frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # Convertir el frame de BGR a HSV
+    # Detección de la base (contorno)
+    frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convertir el frame de BGR a escala de grises
+    base = detect_base(frame_grey)
+    cv2.imshow("Base", base)
 
+    # Detección de las piezas (colores)
+    frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # Convertir el frame de BGR a HSV
     for name, lower, upper, colorBGR in COLORS:
         mask = detect_color(frame, frame_HSV, name, lower, upper, colorBGR)
         mask = cv2.resize(mask, None, fx=IMG_SCALE, fy=IMG_SCALE, interpolation=cv2.INTER_LINEAR)
-        cv2.imshow("Mask " + name, mask)
+        #cv2.imshow("Mask " + name, mask)
 
     # Mostrar webcam y máscara
     frame = cv2.resize(frame, None, fx=IMG_SCALE, fy=IMG_SCALE, interpolation=cv2.INTER_LINEAR)
     cv2.imshow("Webcam", frame)
+
+    # STACK IMAGES
 
     if cv2.waitKey(1) == ord('q'):
         break
