65d5b3d5f4714cab08cf5f3280941fc27f1c896d
ESPITY <132686177+ESPITY@users.noreply.github.com>
2026-05-13 02:13:20 +0200

    Añadido el color lila y modificado el rango del amarillo

--- code/config.py
+++ code/config.py
@@ -5,11 +5,12 @@ import numpy as np
 # Color: Nombre, lower HSV, upper HSV, color BGR
 COLORS = [
     ["Rojo", np.array([0, 200, 20]), np.array([10, 255, 255]), (0, 0, 255)],
-    ["Amarillo", np.array([15, 200, 20]), np.array([25, 255, 255]), (0, 255, 255)],
+    ["Amarillo", np.array([15, 180, 20]), np.array([25, 255, 255]), (0, 255, 255)],
     ["Azul", np.array([100, 200, 20]), np.array([120, 255, 255]), (255, 0, 0)],
     ["Verde", np.array([40, 150, 20]), np.array([100, 255, 255]), (0, 255, 0)],
     ["Negro", np.array([0, 0, 0]), np.array([179, 255, 40]), (105, 105, 105)],
-    ["Naranja", np.array([8, 150, 120]), np.array([20, 255, 255]), (0, 164, 255)]
+    ["Naranja", np.array([8, 150, 120]), np.array([20, 255, 255]), (0, 164, 255)],
+    ["Lila", np.array([120, 30, 130]), np.array([180, 90, 255]), (204, 150, 245)]
 ]
 #["Rojo", np.array([0, 200, 20]), np.array([5, 255, 255]), (0, 0, 255)],
 
